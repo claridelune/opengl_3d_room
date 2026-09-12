c5aaaf76650c5f1919b5b178b2a43d134e664267
DevJPac18 <j.pachas18@gmail.com>
2026-09-11 22:08:11 -0500

    Add animation variables and auxiliary functions to Scene

--- include/Scene.hpp
+++ include/Scene.hpp
@@ -9,10 +9,25 @@ class Scene
 	private:
 		Cone cone;
 		OBJModel model;
+		// Variables de animación
+    	float minuteAngle;
+    	float lampAngle;
+
+    	// Funciones auxiliares
+    	void drawCube(float x, float y, float z) const;
 
 	public:
 		Scene();
 		void draw() const;
+
+		// Objetos JesusP
+	    void drawComoda() const;
+   		void drawLampara() const;
+    	void drawEstante() const;
+    	void drawReloj() const;
+
+    	// Animación
+    	void update();
 };
 
 #endif
